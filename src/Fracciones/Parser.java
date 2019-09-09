9302cde6800730a4588f7752fed415b77ddee19f
saul <54ulxd@gmail.com>
2019-09-09 13:38:48 -0500

    execute token finished

--- src/Fracciones/Parser.java
+++ src/Fracciones/Parser.java
@@ -1,7 +1,7 @@
 
 
 
-public enum TokenType{Null,ShortNumeric,LongNumeric,Conjuction,MathOperator};
+public enum TokenType{Null,Numeric,TextualNumeric,Conjuction,MathOperator};
 
 public class Token
 {
@@ -25,62 +25,64 @@ public class Parser
     private Token[]  TokenDataBase = 
     {
         //Numerales
-        {"cero",0,TokenType.ShortNumeric},
-        {"un",1,TokenType.ShortNumeric},
-        {"dos",2,TokenType.ShortNumeric},
-        {"tres",3,TokenType.ShortNumeric},
-        {"cuatro",4,TokenType.ShortNumeric},
-        {"cinco",5,TokenType.ShortNumeric},
-        {"seis",5,TokenType.ShortNumeric},
-        {"siete",7,TokenType.ShortNumeric},
-        {"ocho",8,TokenType.ShortNumeric},
-        {"nueve",9,TokenType.ShortNumeric},
-        {"diez",10,TokenType.ShortNumeric},
-        {"once",11,TokenType.ShortNumeric},
-        {"doce",12,TokenType.ShortNumeric},
-        {"trece",13,TokenType.ShortNumeric},
-        {"catorce",14,TokenType.ShortNumeric},
-        {"quince",15,TokenType.ShortNumeric},
-        {"dieciseis",16,TokenType.ShortNumeric},
-        {"diecisiete",17,TokenType.ShortNumeric},
-        {"dieciocho",18,TokenType.ShortNumeric},
-        {"diecinueve",19,TokenType.ShortNumeric},
-        {"veinte",20,TokenType.ShortNumeric},
-        {"veintiun",21,TokenType.ShortNumeric},
-        {"veintidos",21,TokenType.ShortNumeric},
-        {"veintitres",23,TokenType.ShortNumeric},
-        {"veinticuatro",24,TokenType.ShortNumeric},
-        {"veinticinco",25,TokenType.ShortNumeric},
-        {"veintiseis",26,TokenType.ShortNumeric},
-        {"veintisiete",27,TokenType.ShortNumeric},
-        {"veintiocho",28,TokenType.ShortNumeric},
-        {"veintinueve",29,TokenType.ShortNumeric},
-        {"treinta",30,TokenType.ShortNumeric},
-        {"cuarenta",40,TokenType.ShortNumeric},
-        {"cincuenta",50,TokenType.ShortNumeric},
-        {"sesenta",60,TokenType.ShortNumeric},
-        {"setenta",70,TokenType.ShortNumeric},
-        {"ochenta",80,TokenType.ShortNumeric},
-        {"noventa",90,TokenType.ShortNumeric},
-        {"cien",100,TokenType.ShortNumeric},
+        {"cero",0,TokenType.Numeric},
+        {"un",1,TokenType.Numeric},
+        {"dos",2,TokenType.Numeric},
+        {"tres",3,TokenType.Numeric},
+        {"cuatro",4,TokenType.Numeric},
+        {"cinco",5,TokenType.Numeric},
+        {"seis",5,TokenType.Numeric},
+        {"siete",7,TokenType.Numeric},
+        {"ocho",8,TokenType.Numeric},
+        {"nueve",9,TokenType.Numeric},
+        {"diez",10,TokenType.Numeric},
+        {"once",11,TokenType.Numeric},
+        {"doce",12,TokenType.Numeric},
+        {"trece",13,TokenType.Numeric},
+        {"catorce",14,TokenType.Numeric},
+        {"quince",15,TokenType.Numeric},
+        {"dieciseis",16,TokenType.Numeric},
+        {"diecisiete",17,TokenType.Numeric},
+        {"dieciocho",18,TokenType.Numeric},
+        {"diecinueve",19,TokenType.Numeric},
+        {"veinte",20,TokenType.Numeric},
+        {"veintiun",21,TokenType.Numeric},
+        {"veintidos",21,TokenType.Numeric},
+        {"veintitres",23,TokenType.Numeric},
+        {"veinticuatro",24,TokenType.Numeric},
+        {"veinticinco",25,TokenType.Numeric},
+        {"veintiseis",26,TokenType.Numeric},
+        {"veintisiete",27,TokenType.Numeric},
+        {"veintiocho",28,TokenType.Numeric},
+        {"veintinueve",29,TokenType.Numeric},
+        {"treinta",30,TokenType.Numeric},
+        {"cuarenta",40,TokenType.Numeric},
+        {"cincuenta",50,TokenType.Numeric},
+        {"sesenta",60,TokenType.Numeric},
+        {"setenta",70,TokenType.Numeric},
+        {"ochenta",80,TokenType.Numeric},
+        {"noventa",90,TokenType.Numeric},
+        {"cien",100,TokenType.Numeric},
         //Denominadores(los que terminan en avo son denominadores tambien)
-        {"enteros",1,TokenType.LongNumeric},
-        {"medios",2,TokenType.LongNumeric},
-        {"tercios",3,TokenType.LongNumeric},
-        {"cuartos",4,TokenType.LongNumeric},
-        {"quintos",5,TokenType.LongNumeric},
-        {"sextos",6,TokenType.LongNumeric},
-        {"septimos",7,TokenType.LongNumeric},
-        {"octavos",8,TokenType.LongNumeric},
-        {"novenos",9,TokenType.LongNumeric},
-        {"decimos",10,TokenType.LongNumeric},
-        {"centesimos",100,TokenType.LongNumeric},
+        {"enteros",1,TokenType.Numeric},
+        {"medios",2,TokenType.Numeric},
+        {"tercios",3,TokenType.Numeric},
+        {"cuartos",4,TokenType.Numeric},
+        {"quintos",5,TokenType.Numeric},
+        {"sextos",6,TokenType.Numeric},
+        {"septimos",7,TokenType.Numeric},
+        {"octavos",8,TokenType.Numeric},
+        {"novenos",9,TokenType.Numeric},
+        {"decimos",10,TokenType.Numeric},
+        {"centesimos",100,TokenType.Numeric},
         //System tokens
         {"y",0,TokenType.Conjuction},
         {"mas",0,TokenType.MathOperator},
         {"menos",1,TokenType.MathOperator},
         {"por",2,TokenType.MathOperator},
         {"entre",3,TokenType.MathOperator},
+        //Runtime Types
+        {"avo",0,TokenType.LastNumerator}
     };
 
     private void PushOperatorStack(Fraccion value)
@@ -103,50 +105,82 @@ public class Parser
         return ParserStack[ParserIndex--];
     }
     
-    private Tokens[] ParseStrings(String[] stringsAsTokens)
+    private Tokens[] ParseTokens(String[] stringsAsTokens)
     {
         //Checks if the strings matches in some of the three maps
-        //if has a match in the short numeric, give them the related token.
+        //if 
+
+        //cuatro quintos entre dieciseis quintos
+        // push(4)
+        // push(5)
+        // a=pop, b=pop
+        // res = a.div(b)
+        // strRes = ParseFraction(res)
+
+        // cincuenta y tres  setenta y unoavos
+
+       //push(50)
+       //push(pop()+next)
+       //push(70) 
+       //push numeral 
+
     }
 
-    private Fraccion ParseTokenArray(Tokens[] tokens)
+
+    private Fraccion Execute(Tokens[] tokens,int indx=0)
     {
-        TokenCounter =0;
+        TokenCounter =indx;
+        int terminosAgarrados=0;
 
         while(pc<tokens.length)
         { 
+
             Token currentT = tokens[TokenCounter];
+
+            //dos terminos agregados, si es diferente de 0 sigmifica que
+            //los valores que entran son puramente valores despues de un operador a/b mas {a/b}
+            
+            if(terminosAgarrados==2 && indx!=0)
+            {
+              int b=PopStack();
+              int a=PopStack();
+              return new Fracccion(a,b);
+            }
+
             switch(currentT.type)
             {
-                case TokenType.ShortNumeric:
-                PushStack(currentT.value);
-                TokenCounter++;
-                break;
-                
-                case TokenType.LongNumeric:
-                PushStack(currentT.value);
-                TokenCounter++;
+                case TokenType.Numeric:
+                    PushStack(currentT.value);
+                    TokenCounter++;
+                    terminosAgarrados++;
                 break;
 
                 case TokenType.Conjuction:
-                PushStack(PopStack()+ tokens[TokenCounter+1].value); 
-                TokenCounter+=2;
+                    PushStack(PopStack()+ tokens[TokenCounter+1].value); 
+                    TokenCounter+=2;
+                    terminosAgarrados++;
                 break;
 
                 case TokenType.MathOperator:
+
+                    int b = PopStack();
+                    int a = PopStack();
+                    TokenCounter++;
+                    PushOperatorStack(new Fraccion (a,b));
+                    PushOperatorStack(new Fraccion(Execute(tokens,TokenCounter)));
+                    Fraccion bf= PopOperatorStack();
+                    Fraccion af= PopOperatorStack();
+
                     switch(currentT.value)
                     {
                         case 0: //Suma
-                        break;
-
+                            return af.add(bf);
                         case 1://Resta
-                        break;
-
+                            return af.sub(bf);
                         case 2://Multiplicacion
-                        break;
-
+                            return af.mul(bf);
                         case 3://Division
-                        break;
+                            return af.div(bf);
                     }
                 break;
             }
